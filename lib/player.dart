127112b092a26c403eecd4d6c935ae66b9e865a6
youbudong <alanal@qq.com>
2022-02-21 10:47:33 +0800

    player

---
+++ lib/player.dart
@@ -0,0 +1,33 @@
+import 'package:fijkplayer/fijkplayer.dart';
+import 'package:flutter/material.dart';
+
+class Player extends StatefulWidget {
+  const Player({ Key? key }) : super(key: key);
+
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  final FijkPlayer player = FijkPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setDataSource(
+        "http://39.134.115.163:8080/PLTV/88888910/224/3221225632/index.m3u8",
+        autoPlay: true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(
+      child: FijkView(player: player),
+    ),);
+  }
+}
